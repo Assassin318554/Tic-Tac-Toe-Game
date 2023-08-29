df800c0b1c24c6da67a60f3baf3b48f9ff3b8f89
Nahin Intesher <124162145+Assassin318554@users.noreply.github.com>
2023-08-29 21:08:18 +0600

    Add files via upload

--- DoesItMatch.java
+++ DoesItMatch.java
@@ -1,53 +1,53 @@
-package OwnPractice.tictactoe;
+package DewanSir.tictactoe;
 
 import java.awt.*;
 
 public class DoesItMatch {
     boolean check(TicTacToe ticTacToe) {
-        if (ticTacToe.buttons[0].getText() != "" && ticTacToe.buttons[0].getText() == ticTacToe.buttons[1].getText() &&
-                ticTacToe.buttons[1].getText() == ticTacToe.buttons[2].getText()) {
+        if (ticTacToe.buttons[0].getLabel() != "" && ticTacToe.buttons[0].getLabel() == ticTacToe.buttons[1].getLabel() &&
+                ticTacToe.buttons[1].getLabel() == ticTacToe.buttons[2].getLabel()) {
             ticTacToe.buttons[0].setBackground(Color.blue);
             ticTacToe.buttons[1].setBackground(Color.blue);
             ticTacToe.buttons[2].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[3].getText() != "" && ticTacToe.buttons[3].getText() == ticTacToe.buttons[4].getText() &&
-                ticTacToe.buttons[4].getText() == ticTacToe.buttons[5].getText()) {
+        } else if (ticTacToe.buttons[3].getLabel() != "" && ticTacToe.buttons[3].getLabel() == ticTacToe.buttons[4].getLabel() &&
+                ticTacToe.buttons[4].getLabel() == ticTacToe.buttons[5].getLabel()) {
             ticTacToe.buttons[3].setBackground(Color.blue);
             ticTacToe.buttons[4].setBackground(Color.blue);
             ticTacToe.buttons[5].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[6].getText() != "" && ticTacToe.buttons[6].getText() == ticTacToe.buttons[7].getText() &&
-                ticTacToe.buttons[7].getText() == ticTacToe.buttons[8].getText()) {
+        } else if (ticTacToe.buttons[6].getLabel() != "" && ticTacToe.buttons[6].getLabel() == ticTacToe.buttons[7].getLabel() &&
+                ticTacToe.buttons[7].getLabel() == ticTacToe.buttons[8].getLabel()) {
             ticTacToe.buttons[6].setBackground(Color.blue);
             ticTacToe.buttons[7].setBackground(Color.blue);
             ticTacToe.buttons[8].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[0].getText() != "" && ticTacToe.buttons[0].getText() == ticTacToe.buttons[3].getText() &&
-                ticTacToe.buttons[3].getText() == ticTacToe.buttons[6].getText()) {
+        } else if (ticTacToe.buttons[0].getLabel() != "" && ticTacToe.buttons[0].getLabel() == ticTacToe.buttons[3].getLabel() &&
+                ticTacToe.buttons[3].getLabel() == ticTacToe.buttons[6].getLabel()) {
             ticTacToe.buttons[0].setBackground(Color.blue);
             ticTacToe.buttons[3].setBackground(Color.blue);
             ticTacToe.buttons[6].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[1].getText() != "" && ticTacToe.buttons[1].getText() == ticTacToe.buttons[4].getText() &&
-                ticTacToe.buttons[4].getText() == ticTacToe.buttons[7].getText()) {
+        } else if (ticTacToe.buttons[1].getLabel() != "" && ticTacToe.buttons[1].getLabel() == ticTacToe.buttons[4].getLabel() &&
+                ticTacToe.buttons[4].getLabel() == ticTacToe.buttons[7].getLabel()) {
             ticTacToe.buttons[1].setBackground(Color.blue);
             ticTacToe.buttons[4].setBackground(Color.blue);
             ticTacToe.buttons[7].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[2].getText() != "" && ticTacToe.buttons[2].getText() == ticTacToe.buttons[5].getText() &&
-                ticTacToe.buttons[5].getText() == ticTacToe.buttons[8].getText()) {
+        } else if (ticTacToe.buttons[2].getLabel() != "" && ticTacToe.buttons[2].getLabel() == ticTacToe.buttons[5].getLabel() &&
+                ticTacToe.buttons[5].getLabel() == ticTacToe.buttons[8].getLabel()) {
             ticTacToe.buttons[2].setBackground(Color.blue);
             ticTacToe.buttons[5].setBackground(Color.blue);
             ticTacToe.buttons[8].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[0].getText() != "" && ticTacToe.buttons[0].getText() == ticTacToe.buttons[4].getText() &&
-                ticTacToe.buttons[4].getText() == ticTacToe.buttons[8].getText()) {
+        } else if (ticTacToe.buttons[0].getLabel() != "" && ticTacToe.buttons[0].getLabel() == ticTacToe.buttons[4].getLabel() &&
+                ticTacToe.buttons[4].getLabel() == ticTacToe.buttons[8].getLabel()) {
             ticTacToe.buttons[0].setBackground(Color.blue);
             ticTacToe.buttons[4].setBackground(Color.blue);
             ticTacToe.buttons[8].setBackground(Color.blue);
             return true;
-        } else if (ticTacToe.buttons[2].getText() != "" && ticTacToe.buttons[2].getText() == ticTacToe.buttons[4].getText() &&
-                ticTacToe.buttons[4].getText() == ticTacToe.buttons[6].getText()) {
+        } else if (ticTacToe.buttons[2].getLabel() != "" && ticTacToe.buttons[2].getLabel() == ticTacToe.buttons[4].getLabel() &&
+                ticTacToe.buttons[4].getLabel() == ticTacToe.buttons[6].getLabel()) {
             ticTacToe.buttons[2].setBackground(Color.blue);
             ticTacToe.buttons[4].setBackground(Color.blue);
             ticTacToe.buttons[6].setBackground(Color.blue);
--- Main.java
+++ Main.java
@@ -1,32 +1,30 @@
-package OwnPractice.tictactoe;
+package DewanSir.tictactoe;
 
-import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
-
-class TicTacToe implements ActionListener {
+class TicTacToe implements ActionListener, WindowListener {
     static int count = 9;
     static int win = 0;
-    JFrame jframe = new JFrame();
-    JButton []buttons = new JButton[9];
-    JButton reset = new JButton("Reset Game");
-    JLabel name = new JLabel("Tic-Tac-Toe");
-    JPanel jpnel = new JPanel();
-    JLabel player1 = new JLabel("Player 1:  (X)");
-    JLabel player2 = new JLabel("Player 2:  (O)");
-    JLabel move = new JLabel("Player 1's move");
-    JLabel winner = new JLabel("");
+    Frame frame = new Frame();
+    Image icon = Toolkit.getDefaultToolkit().getImage("D:\\Java\\Java Workshop\\src\\DewanSir\\tictactoe\\icon.png");
+    Button []buttons = new Button[9];
+    Button reset = new Button("Reset Game");
+    Label name = new Label("Tic-Tac-Toe");
+    Panel panel = new Panel();
+    Label player1 = new Label("Player 1:  (X)");
+    Label player2 = new Label("Player 2:  (O)");
+    Label move = new Label("Player 1's move");
+    Label winner = new Label("");
     public TicTacToe(){
-        jframe.setLocation(50,50);
-        jframe.setSize(600,700);
-        jframe.setTitle("Tic-Tac-Toe");
-        jframe.setResizable(false);
-        jframe.getContentPane().setBackground(Color.decode("#2EFEC8"));
+        frame.setLocation(400,50);
+        frame.setSize(600,700);
+        frame.setTitle("Tic-Tac-Toe");
+        frame.setResizable(false);
+        frame.setBackground(Color.decode("#2EFEC8"));
+        frame.setIconImage(icon);
         for(int i = 0; i < 9; i++){
-            buttons[i] = new JButton();
+            buttons[i] = new Button();
         }
         buttons[0].setBounds(150,200,100,100);
         buttons[1].setBounds(250,200,100,100);
@@ -38,16 +36,15 @@ class TicTacToe implements ActionListener {
         buttons[7].setBounds(250,400,100,100);
         buttons[8].setBounds(350,400,100,100);
         for(int i = 0; i < 9; i++){
-            buttons[i].setBorder(new LineBorder(Color.BLACK, 2));
             buttons[i].setBackground(Color.darkGray);
             buttons[i].setForeground(Color.WHITE);
             buttons[i].addActionListener(this::actionPerformed);
-            jframe.add(buttons[i]);
-            buttons[i].setFocusPainted(false);
+            frame.add(buttons[i]);
+            buttons[i].setFocusable(false);
         }
-        player1.setBounds(170,100,200,25);
-        player2.setBounds(350,100,200,25);
-        move.setBounds(215,150,200,30);
+        player1.setBounds(170,100,170,25);
+        player2.setBounds(350,100,300,25);
+        move.setBounds(200,150,230,30);
         player1.setFont(new Font("Cascadia Code",Font.BOLD,15));
         player2.setFont(new Font("Cascadia Code",Font.BOLD,15));
         move.setFont(new Font("Cascadia Code",Font.BOLD,25));
@@ -57,63 +54,67 @@ class TicTacToe implements ActionListener {
         reset.setBounds(210,580,180,50);
         reset.setBackground(Color.BLACK);
         reset.setForeground(Color.WHITE);
-        reset.setBorder(new LineBorder(Color.BLACK, 2));
 
-        reset.setFocusPainted(false);
+        reset.setFocusable(false);
         reset.setFont(new Font("Times New Roman",Font.BOLD,20));
         reset.addActionListener(this::actionPerformed);
-        name.setBounds(200,-10,300,100);
+        name.setBounds(200,15,300,100);
         name.setFont(new Font("Times New Roman",Font.BOLD,40));
 
-        jpnel.setOpaque(true);
-        jframe.add(reset);
-        jframe.add(player1);
-        jframe.add(player2);
-        jframe.add(move);
-        jframe.add(winner);
-        jframe.add(jpnel);
-        jframe.add(name);
-        jframe.setLayout(null);
-        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
-        jframe.setVisible(true);
+        panel.setFocusable(false);
+        frame.add(reset);
+        frame.add(player1);
+        frame.add(player2);
+        frame.add(move);
+        frame.add(winner);
+        frame.add(panel);
+        frame.add(name);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.addWindowListener(this);
+        frame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e){
         DoesItMatch doesItMatch = new DoesItMatch();
         for (int i = 0; i < 9; i++){
             if(e.getSource() == reset){
-                reset.setText("Reset Game");
+                reset.setLabel("Reset Game");
                 for(int j = 0; j < 9; j++) {
-                    buttons[j].setText("");
+                    buttons[j].setLabel("");
                     buttons[j].setBackground(Color.darkGray);
                     buttons[i].setForeground(Color.WHITE);
                 }
                 winner.setText("");
                 win = 0;
                 count = 9;
-                move.setText("Player 1's move");
+                move.setText("Player 1's move (X)");
             }
             else if (e.getSource() != reset && win!=1){
-                if(e.getSource() == buttons[i] && buttons[i].getText() == "" && count%2 != 0){
+                if(e.getSource() == buttons[i] && buttons[i].getLabel() == "" && count%2 != 0){
                     buttons[i].setFont(new Font("Sans",Font.PLAIN,50));
-                    buttons[i].setText("X");
-                    move.setText("Player 2's move");
+                    buttons[i].setLabel("X");
+                    move.setText("Player 2's move (O)");
                     if(doesItMatch.check(this) == true){
-                        winner.setBounds(230,520,200,50);
-                        winner.setText("Player 1 wins!");
+                        winner.setBounds(200,520,200,50);
+                        move.setText("");
+                        winner.setText("Player 1 (X) wins!");
+                        reset.setLabel("New Game");
                         win = 1;
                         count = 9;
                         break;
                     }
                     count--;
                 }
-                else if(e.getSource() == buttons[i] && buttons[i].getText() == "" && count%2 == 0){
+                else if(e.getSource() == buttons[i] && buttons[i].getLabel() == "" && count%2 == 0){
                     buttons[i].setFont(new Font("Sans",Font.PLAIN,50));
-                    buttons[i].setText("O");
-                    move.setText("Player 1's move");
+                    buttons[i].setLabel("O");
+                    move.setText("Player 1's move (X)");
                     if(doesItMatch.check(this) == true){
-                        winner.setBounds(230,520,200,50);
-                        winner.setText("Player 2 wins!");
+                        move.setText("");
+                        winner.setBounds(200,520,200,50);
+                        winner.setText("Player 2 (O) wins!");
+                        reset.setLabel("New Game");
                         win = 1;
                         count = 9;
                         break;
@@ -125,11 +126,21 @@ class TicTacToe implements ActionListener {
         if(win == 0 && count==0){
             winner.setBounds(270,520,200,50);
             winner.setText("Draw!");
+            reset.setLabel("New Game");
         }
     }
+
+
+    public void windowOpened(WindowEvent e) {}
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+    public void windowClosed(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
 }
-
-
 
 public class Main {
     public static void main(String[] args) {
